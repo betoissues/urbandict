c947717b604895bc71850537dd8728aaa002ea96
alberto castillo gordón <acastillogordon@gmail.com>
2017-12-12 13:49:46 -0500

    Accepts more than 1 CLI arg

--- urbandict.py
+++ urbandict.py
@@ -4,9 +4,13 @@ import sys
 import json
 from urllib.request import urlopen
 
-if __name__ == "__main__":
+def main():
     if(len(sys.argv) > 1):
-        word = sys.argv[1]
+        word = '%20'.join(sys.argv[1:])
         data = json.load(urlopen('http://api.urbandictionary.com/v0/define?term=' + word))
         for index in range(3):
             print(str(index+1) + '. ' + data['list'][index]['definition'] + '\n')
+
+
+if __name__ == "__main__":
+    main()
